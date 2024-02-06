cbb23127db2582265766f6bce034f63c0e1cb6cd
Ycanzzay <steam3bera228@gmail.com>
2024-02-06 19:11:03 +0300

    add 2 methods: Array.countIndexEqualAvgDeviation Array.countIndexEqualAvgDeviation

--- src/by/itstep/aniskovich/java/stage14/level/c/model/Array.java
+++ src/by/itstep/aniskovich/java/stage14/level/c/model/Array.java
@@ -90,4 +90,53 @@ public class Array {
 
         return count;
     }
+
+    public static int countIndexEqualAvgDeviation(int[] array,
+                                                  double deviation) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+        double avg = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            avg += array[i];
+        }
+        avg = avg / array.length;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == avg - deviation
+                    || array[i] == avg + deviation) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int countIndexEqualAvgGeomDeviation(int[] array,
+                                                      double deviation) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+        double avg_geom = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            avg_geom *= array[i];
+        }
+
+        avg_geom = Math.pow(avg_geom, 1.0 / array.length);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == avg_geom - deviation
+                    || array[i] == avg_geom + deviation ) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
