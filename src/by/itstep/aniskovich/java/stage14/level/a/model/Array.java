9f4fce4016cc122826a848d95aff6cc36ace71ff
Aniskovichav <aniskovichav@gmail.com>
2024-02-06 14:50:15 +0300

    add Array.java

---
+++ src/by/itstep/aniskovich/java/stage14/level/a/model/Array.java
@@ -0,0 +1,33 @@
+package by.itstep.aniskovich.java.stage14.level.a.model;
+
+public class Array {
+    public static int countNonzeroIndex(int[] array) {
+
+        return -1;
+    }
+
+    public static int countEvenIndex(int[] array) {
+
+        return -1;
+    }
+
+    public static int countPositiveIndex(int[] array) {
+
+        return -1;
+    }
+
+    public static int countZeroIndex(int[] array) {
+
+        return -1;
+    }
+
+    public static int countOddIndex(int[] array) {
+
+        return -1;
+    }
+
+    public static int countNegativeIndex(int[] array) {
+
+        return -1;
+    }
+}
