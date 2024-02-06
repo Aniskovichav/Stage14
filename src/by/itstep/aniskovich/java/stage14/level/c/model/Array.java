a9b4329a3dd24a2664c2aad7e50409291dd02da7
Aniskovichav <aniskovichav@gmail.com>
2024-02-06 18:00:22 +0300

    add Array.java level.c with 4 methods

---
+++ src/by/itstep/aniskovich/java/stage14/level/c/model/Array.java
@@ -0,0 +1,93 @@
+package by.itstep.aniskovich.java.stage14.level.c.model;
+
+public class Array {
+    public static int countIndexOverAvg(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+        double avg = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            avg += array[i];
+        }
+        avg = avg / array.length;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > avg) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int countIndexOverAvgGeom(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+        double avg_geom = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            avg_geom *= array[i];
+        }
+
+        avg_geom = Math.pow(avg_geom, 1.0 / array.length);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > avg_geom) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int countIndexUnderAvg(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+        double avg = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            avg += array[i];
+        }
+        avg = avg / array.length;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < avg) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int countIndexUnderAvgGeom(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+        double avg_geom = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            avg_geom *= array[i];
+        }
+
+        avg_geom = Math.pow(avg_geom, 1.0 / array.length);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < avg_geom) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
