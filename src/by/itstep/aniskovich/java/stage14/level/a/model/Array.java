e36d1e4dfdb4f35a03262c3947703435363e62d0
Aniskovichav <aniskovichav@gmail.com>
2024-02-06 14:58:09 +0300

    add Array.java with methods

--- src/by/itstep/aniskovich/java/stage14/level/a/model/Array.java
+++ src/by/itstep/aniskovich/java/stage14/level/a/model/Array.java
@@ -2,32 +2,73 @@ package by.itstep.aniskovich.java.stage14.level.a.model;
 
 public class Array {
     public static int countNonzeroIndex(int[] array) {
+        int count = 0;
 
-        return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static int countEvenIndex(int[] array) {
+        int count = 0;
 
-        return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static int countPositiveIndex(int[] array) {
+        int count = 0;
 
-        return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static int countZeroIndex(int[] array) {
+        int count = 0;
 
-        return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static int countOddIndex(int[] array) {
+        int count = 0;
 
-        return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static int countNegativeIndex(int[] array) {
+        int count = 0;
 
-        return -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
