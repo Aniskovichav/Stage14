cde0fe0cc85c4fe708ac5b511eac67408d87c817
Timmi1015 <aniskovichav@gmail.com>
2024-02-11 22:05:13 +0300

    add classes controller level.a

--- src/by/itstep/aniskovich/java/stage14/level/a/controller/Task03.java
+++ src/by/itstep/aniskovich/java/stage14/level/a/controller/Task03.java
@@ -1,7 +1,21 @@
 package by.itstep.aniskovich.java.stage14.level.a.controller;
 
+import by.itstep.aniskovich.java.stage14.level.a.model.Array;
+import by.itstep.aniskovich.java.stage14.level.a.util.ArrayPositiveUI;
+import by.itstep.aniskovich.java.stage14.level.util.ArrayInit;
+import by.itstep.aniskovich.java.stage14.level.view.ConsolePrinter;
+
+import java.util.Arrays;
+
 public class Task03 {
     public static void main(String[] args) {
+        int[] array = new int[ArrayInit.randomSizeArray()];
 
+        ArrayInit.randomInitElement(array);
+
+        ConsolePrinter.print(Arrays.toString(array) + "\t");
+
+        ConsolePrinter.print(ArrayPositiveUI.format
+                (Array.countPositiveElement(array)));
     }
 }
--- src/by/itstep/aniskovich/java/stage14/level/a/controller/Task05.java
+++ src/by/itstep/aniskovich/java/stage14/level/a/controller/Task05.java
@@ -1,7 +1,21 @@
 package by.itstep.aniskovich.java.stage14.level.a.controller;
 
+import by.itstep.aniskovich.java.stage14.level.a.model.Array;
+import by.itstep.aniskovich.java.stage14.level.a.util.ArrayOddUI;
+import by.itstep.aniskovich.java.stage14.level.util.ArrayInit;
+import by.itstep.aniskovich.java.stage14.level.view.ConsolePrinter;
+
+import java.util.Arrays;
+
 public class Task05 {
     public static void main(String[] args) {
+        int[] array = new int[ArrayInit.randomSizeArray()];
 
+        ArrayInit.randomInitElement(array);
+
+        ConsolePrinter.print(Arrays.toString(array) + "\t");
+
+        ConsolePrinter.print(ArrayOddUI.format
+                (Array.countOddElement(array)));
     }
 }
